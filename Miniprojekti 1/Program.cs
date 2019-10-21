03e055fd9abe9b920586aba56d8e958ccae0e0ae
Ria Ahlström <ria.ahlstrom@gmail.com>
2019-10-21 13:39:33 +0300

    Repository luotu

--- Miniprojekti 1/Program.cs
+++ Miniprojekti 1/Program.cs
@@ -6,7 +6,7 @@ namespace Miniprojekti_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
