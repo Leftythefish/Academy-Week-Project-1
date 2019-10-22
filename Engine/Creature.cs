694a4cd92df111894151d95d26f536cb2a406aac
Ria Ahlström <ria.ahlstrom@gmail.com>
2019-10-22 11:37:00 +0300

    Basic Class Properties added

--- Engine/Creature.cs
+++ Engine/Creature.cs
@@ -13,5 +13,35 @@ namespace Engine
         /// Maximum health
         /// etc
         ///</summary>
+        ///
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int cur_health;
+
+        public int Cur_Health
+        {
+            get { return cur_health; }
+            set { cur_health = value; }
+        }
+
+        private int max_health;
+        public int Max_Health
+        {
+            get { return max_health; }
+            set { max_health = value; }
+        }
+               
+        public Creature(string name, int maximum_health)
+        {
+            this.Name = name;
+            this.Max_Health = maximum_health;
+            this.Cur_Health = maximum_health;
+        }
     }
 }
